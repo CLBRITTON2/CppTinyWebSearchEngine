af84371cb76db7d457c0b4f696611cb5d0df85ce
Chris Britton <christopher.l.britton@outlook.com>
2024-07-30 12:16:17 -0400

    Add base level context for inverted index header
    Define methods to index web pages, and search through indexed web pages with a given query

--- InvertedIndex.h
+++ InvertedIndex.h
@@ -1,4 +1,19 @@
 #pragma once
+#include "WebPage.h"
+#include <vector>
+#include <map>
 
-// Needs a method for adding web pages to a collection - takes a web page object and returns nothing
-// Needs a method to get web pages from said collection using a search term that returns a collection of applicable web pages
+class InvertedIndex
+{
+public:
+	// Needs a method for adding web pages to a collection - takes a web page object and returns nothing
+	// Breaks the file down into individual words, updates inverted index with frequency of each word in the page
+	void IndexWebPageContent(const WebPage& webPage);
+
+	// Needs a method to get web pages from said collection using a search term that returns a collection of applicable web pages
+	// Returns a vector containing the page ID paired with the number of times the queried word appeared in the page
+	std::vector<std::pair<int, int>> Search(const std::string& query);
+
+private:
+	std::map<std::string, std::map<int, int>> _index;
+};
