22eca4b44ae36305461dd80f59c8a714471c9ad8
Chris Britton <christopher.l.britton@outlook.com>
2024-07-30 17:34:17 -0400

    Define search engine basics

---
+++ SearchEngine.cpp
@@ -0,0 +1,10 @@
+#include "SearchEngine.h"
+
+void SearchEngine::IndexWebPage(const WebPage& webPage)
+{
+}
+
+std::vector<std::pair<WebPage, int>> SearchEngine::Search(const std::string& query, int numberOfResults)
+{
+	return std::vector<std::pair<WebPage, int>>();
+}
--- SearchEngine.h
+++ SearchEngine.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include "WebPage.h"
+#include "WebPageCrawler.h"
+#include "InvertedIndex.h"
+
+class SearchEngine
+{
+public:
+	void IndexWebPage(const WebPage& webPage);
+	std::vector<std::pair<WebPage, int>> Search(const std::string& query, int numberOfResults);
+
+private:
+	WebPageCrawler _crawler;
+	InvertedIndex _index;
+};
