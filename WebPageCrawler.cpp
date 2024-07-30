3f1a7d730eaa7768ccc31abfa24caa2e53f88738
Chris Britton <christopher.l.britton@outlook.com>
2024-07-30 17:29:21 -0400

    Add rapidxml.hpp, add html parser to webpagecrawler
    Add method to read html content and parse it into text content for indexing

--- WebPageCrawler.cpp
+++ WebPageCrawler.cpp
@@ -1,6 +1,7 @@
 #include "WebPageCrawler.h"
 #include <curl/curl.h>
 #include <iostream>
+#include <rapidxml.hpp>
 
 void WebPageCrawler::Crawl(const std::string& webPageUrl, WebPage& webPage)
 {
@@ -33,6 +34,40 @@ void WebPageCrawler::Crawl(const std::string& webPageUrl, WebPage& webPage)
 		{
 			std::cerr << "cURL error: " << curl_easy_strerror(httpResponseCode);
 		}
+		else
+		{
+			// Good response extract text from the HTML content to populate the web page content
+			std::string extractedText = ExtractTextFromHtml(webPageHtmlContent);
+			webPage.SetWebPageContent(extractedText);
+		}
+
 		curl_easy_cleanup(curlHandle);
 	}
+}
+
+std::string WebPageCrawler::ExtractTextFromHtml(const std::string& webPageHtmlContent)
+{
+	std::string textContent;
+
+	// Points to a new instance of RapidXML and deletes the instance when it's out of scope
+	std::unique_ptr<rapidxml::xml_document<>> doc(new rapidxml::xml_document<>);
+
+	// Get a pointer to the beginning of the HTML content string
+	// Rapid XML requires a non-const pointer so it must be casted
+	char* start = const_cast<char*>(webPageHtmlContent.c_str());
+	doc->parse<0>(start);
+
+	// Iterate over the nodes in the parsed document
+	rapidxml::xml_node<>* node = doc->first_node();
+	while (node)
+	{	
+		// Only extract the text value of the node if it is a text node
+		if (node->type() == rapidxml::node_data) 
+		{
+			textContent += node->value();
+		}
+		node = node->next_sibling();
+	}
+
+	return textContent;
 }
--- WebPageCrawler.h
+++ WebPageCrawler.h
@@ -6,4 +6,5 @@ class WebPageCrawler
 {
 public:
 	void Crawl(const std::string& url, WebPage& webPage);
+	std::string ExtractTextFromHtml(const std::string& webPageHtmlContent);
 };
