597e96402fab8bf6a115ca6a1bcb0b82539a238b
Chris Britton <christopher.l.britton@outlook.com>
2024-08-01 09:38:39 -0400

    Read URLs from a text file instead of manually adding them in main

--- Main.cpp
+++ Main.cpp
@@ -1,33 +1,46 @@
-using std::chrono::high_resolution_clock;
-using std::chrono::duration_cast;
-using std::chrono::duration;
-using std::chrono::milliseconds;
 #include <iostream>
 #include "SearchEngine.h"
 #include <chrono>
+#include <fstream>
 
 int main()
 {
+    using std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using std::chrono::duration;
+    using std::chrono::milliseconds;
+
     // Create a SearchEngine instance
     SearchEngine searchEngine;
 
-    // Create some WebPage instances
-    WebPage webPage1("https://en.wikipedia.org/wiki/Search_engine", 1);
-    WebPage webPage2("https://en.wikipedia.org/wiki/Software_system", 2);
-    WebPage webPage3("https://en.wikipedia.org/wiki/Software", 3);
-    WebPage webPage4("https://en.wikipedia.org/wiki/Source_code", 4);
+    std::vector<std::string> webPagesFromConfig;
+    std::string line;
+    std::ifstream inFile("WebPages.txt");
+    if (!inFile.is_open())
+    {
+        std::cerr << "Failed to open file." << std::endl;
+        return 1;
+    }
+
+    // Add each URL from config file to a collection
+    while (getline(inFile, line))
+    {
+        webPagesFromConfig.push_back(line);
+    }
 
     auto t1 = high_resolution_clock::now();
-    // Index the web pages
-    searchEngine.IndexWebPage(webPage1);
-    searchEngine.IndexWebPage(webPage2);
-    searchEngine.IndexWebPage(webPage3);
-    searchEngine.IndexWebPage(webPage4);
+    int webPageID = 1;
+    for (auto& line : webPagesFromConfig)
+    {
+        WebPage webPage(line, webPageID);
+        searchEngine.IndexWebPage(webPage);
+        webPageID++;
+    }
     auto t2 = high_resolution_clock::now();
 
     // Getting number of milliseconds as a double. 
     duration<double, std::milli> ms_double = t2 - t1;
-    std::cout << "It took " << ms_double.count() << " ms to index all web pages" << std::endl;
+    std::cout << "It took " << ms_double.count() << " ms to get and index all web pages" << std::endl;
 
     // Search for a token
     std::string query = "the";
--- WebPage.cpp
+++ WebPage.cpp
@@ -1,6 +1,6 @@
 #include "WebPage.h"
 
-WebPage::WebPage(const std::string& url, const int webPageID, const std::string& webPageContent = "")
+WebPage::WebPage(const std::string& url, const int webPageID, const std::string& webPageContent)
 	:_url{ url }, _webPageID{ webPageID }, _webPageContent{ webPageContent }
 {
 }
