74d35fc9242815ca0334f73ff2b57b2af364c8b9
Chris Britton <christopher.l.britton@outlook.com>
2024-08-10 17:22:17 -0400

    Release config tweaks, small change to indexing files
    had to change settings and map search engine to  build against release library files in release mode - check web page url to see if a page has already been indexed - I kind of brute forced this for the time being and it probably wont scale too great. The more web pages get added the longer its going to take to figure out what's been added and what hasnt. Might switch to a hash-map style implementation eventually for detecting indexed URLS

--- WebCrawler/WebPageProcessor.h
+++ WebCrawler/WebPageProcessor.h
@@ -7,13 +7,13 @@ class WebPageProcessor
 {
 public:
 	void ProcessWebPage(WebPage& webPage);
-	std::string GetContentFromHttpRequest(const std::string& webPageUrl, WebPage& webPage);
-	std::string ExtractTextFromHtml(const std::string& webPageContent);
 	void SetLemmatizer(RdrLemmatizer* lemmatizer);
 private:
 	static size_t WriteCallback(void* buffer, size_t bufferSize, size_t numberOfBlocks, void* userData);
 	void SerializeTextContent(lxb_dom_node_t* node, std::string& extractedText);
 	void ExtractUrlsFromWebpage(lxb_dom_node_t* node, std::ofstream& outputFile);
 	void CleanupLXB(lxb_html_parser_t* htmlParser, lxb_html_document_t* document);
+	std::string GetContentFromHttpRequest(const std::string& webPageUrl, WebPage& webPage);
+	std::string ExtractTextFromHtml(const std::string& webPageContent);
 	RdrLemmatizer* _lemmatizer;
 };
