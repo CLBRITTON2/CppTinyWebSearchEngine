ac309e8d3dd061828823d62939194818f2dfa33e
Chris Britton <christopher.l.britton@outlook.com>
2024-08-09 20:33:47 -0400

    Working ish implementation
    Web browser reads binaries to populate required collections - shared library has release and debug build configurations - web crawler mimics original implementation of reading web pages from a txt file - next step is to actually build the web crawler to continuously index pages and build binaries for the search engine

--- CppWebBrowser/Main.cpp
+++ CppWebBrowser/Main.cpp
@@ -73,67 +73,6 @@ int main()
 		std::cerr << "Deserialization failed: " << e.what() << std::endl;
 	}
 
-
-
-	/*
-	_____________________________________________________________________________________________________________________________________________________________
-	TEST LOGIC FOR ADDING WEB PAGES FROM A TXT FILE MANUALLY BEGIN - Comment this entire block out once binaries are created - simulates web crawling
-	_____________________________________________________________________________________________________________________________________________________________
-	*/
-
-	//std::vector<std::string> webPagesFromConfig;
-	//std::string line;
-	//std::ifstream inFile("WebPages.txt");
-	//if (!inFile.is_open())
-	//{
-	//    std::cerr << "Failed to open config file." << std::endl;
-	//    return -1;
-	//}
-
-	//// Add each URL from config file to a collection
-	//while (getline(inFile, line))
-	//{
-	//    webPagesFromConfig.push_back(line);
-	//}
-
-	//// Create an empty vector of threads for each web page
-	//std::vector<std::thread> threads;
-	//int webPageID = 1;
-	//auto timeOne = high_resolution_clock::now();
-
-	//// Basically a C# foreach loop going over each line in the txt file containing urls
-	//for (auto& line : webPagesFromConfig)
-	//{
-	//    // Create a thread for each web page so we can index every page concurrently, adds the thread to threads vector
-	//    // This is a combination of AI/Stack overflow 
-	//    // Might have to look at refactoring this as the project scales because we'll run out of threads
-	//    threads.emplace_back([&searchEngine, &line, webPageID]() {
-	//        WebPage webPage(line, webPageID);
-	//        searchEngine.IndexWebPage(webPage);
-	//        });
-	//    webPageID++;
-	//}
-
-	//for (auto& thread : threads) 
-	//{
-	//    thread.join();
-	//}
-
-	//auto timeTwo = high_resolution_clock::now();
-
-	//// Getting number of milliseconds as a double. 
-	//duration<double, std::milli> msToIndex = timeTwo - timeOne;
-	//std::cout << "It took " << msToIndex.count() << " ms to get and index all web pages" << std::endl;
-	//std::cout << std::endl;
-
-/*
-_____________________________________________________________________________________________________________________________________________________________
-TEST LOGIC FOR ADDING WEB PAGES FROM A TXT FILE MANUALLY END
-_____________________________________________________________________________________________________________________________________________________________
-*/
-
-
-
 	while (true)
 	{
 		std::string query{""};
--- WebCrawler/WebCrawler.h
+++ WebCrawler/WebCrawler.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <string>
 #include <SearchEngineSharedLibrary.h>
 #include "WebPageProcessor.h"
 
