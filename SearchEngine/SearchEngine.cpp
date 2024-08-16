246e143eb8b2520c76bc8c90ebd23f811e18f8a0
Chris Britton <christopher.l.britton@outlook.com>
2024-08-16 16:31:16 -0400

    Moving stuff around in search engine
    spent the day trying to refactor the web crawler to solve the bug that scrapes less web pages than its supposed to - nothing.

--- SearchEngine/SearchEngine.cpp
+++ SearchEngine/SearchEngine.cpp
@@ -12,7 +12,7 @@ void SearchEngine::LoadRepositoryFromBinaryFile(const std::string& fileName)
 	_index.LoadFromBinaryFile(fileName + ".index");
 }
 
-// searchResults: Map of Web Pages with their Titles and Keyword Frequencies
+// Return: Map of Web Pages with their Titles and Keyword Frequencies
 //   - Key: Web Page URL
 //   - Value: Pair containing:
 //       - Web Page Title
@@ -21,41 +21,41 @@ void SearchEngine::LoadRepositoryFromBinaryFile(const std::string& fileName)
 //           - Total Frequency of all Keywords 
 std::unordered_map<std::string, std::pair<std::string, std::pair<std::unordered_map<std::string, int>, int>>> SearchEngine::Search(std::string& query)
 {
-    std::unordered_map<std::string, std::pair<std::string, std::pair<std::unordered_map<std::string, int>, int>>> searchResults;
+	std::unordered_map<std::string, std::pair<std::string, std::pair<std::unordered_map<std::string, int>, int>>> searchResults;
 
-    std::vector<std::string> parsedSearchQuery = _queryParser.Parse(query);
+	std::vector<std::string> parsedSearchQuery = _queryParser.Parse(query);
 
-    for (const auto& word : parsedSearchQuery)
-    {
-        std::vector<std::pair<std::string, int>> tokenFrequencies = _index.GetTokenFrequency(word);
+	for (const auto& word : parsedSearchQuery)
+	{
+		std::vector<std::pair<std::string, int>> tokenFrequencies = _index.GetTokenFrequency(word);
 
-        for (const auto& result : tokenFrequencies)
-        {
-            // Use the web page URL as the key
-            std::string webPageUrl = result.first;
+		for (const auto& result : tokenFrequencies)
+		{
+			// Use the web page URL as the key
+			std::string webPageUrl = result.first;
 
-            // Get the web page title
-            std::string webPageTitle = _webPageRepository.GetWebPageTitle(webPageUrl);
+			// Get the web page title
+			std::string webPageTitle = _webPageRepository.GetWebPageTitle(webPageUrl);
 
-            // Store the web page title
-            searchResults[webPageUrl].first = webPageTitle;
+			// Store the web page title
+			searchResults[webPageUrl].first = webPageTitle;
 
-            // Store the frequency count of the individual word
-            searchResults[webPageUrl].second.first[word] += result.second;
+			// Store the frequency count of the individual word
+			searchResults[webPageUrl].second.first[word] += result.second;
 
-            // Store the total frequency of all keywords from the query
-            searchResults[webPageUrl].second.second += result.second;
-        }
-    }
-    return searchResults;
+			// Store the total frequency of all keywords from the query
+			searchResults[webPageUrl].second.second += result.second;
+		}
+	}
+	return searchResults;
 }
 
 size_t SearchEngine::GetTotalSearchableWebPages()
 {
-    return _webPageRepository.GetWebPageRepositoryCount();
+	return _webPageRepository.GetWebPageRepositoryCount();
 }
 
 std::string SearchEngine::GetWebPageTitleByUrl(const std::string& webPageUrl)
 {
-   return _webPageRepository.GetWebPageTitle(webPageUrl);
+	return _webPageRepository.GetWebPageTitle(webPageUrl);
 }
